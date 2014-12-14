f18f1d7b0ce3d84866aa8b3577c87a72a838cf2f
Grzegorz Pozoga <g.pozoga@gmail.com>
2014-12-14 15:29:16 +0100

    New sidebars for admin and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+
   devise_for :users
+  
+scope '/admin' do
   resources :categories
-
   resources :devices
-
   resources :room_categories
-
   resources :rooms
+end
+
 
   get 'main/index'
 
